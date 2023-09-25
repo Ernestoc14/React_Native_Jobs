93dca074f751ae4eff2a4efb2e95a6d3c149a7b3
Ernestoc14 <descreed34@hotmail.com>
2023-09-24 23:08:49 -0500

    Feat Create a JobFooter Component (Button)

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -4,22 +4,22 @@ import { icons } from "../../../constants";
 
 const Footer = ({ url }) => {
   return (
-    // <View style={styles.container}>
-    //   <TouchableOpacity style={styles.likeBtn}>
-    //     <Image
-    //       source={icons.heartOutline}
-    //       resizeMode="contain"
-    //       style={styles.likeBtnImage}
-    //     />
-    //   </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.likeBtn}>
+        <Image
+          source={icons.heartOutline}
+          resizeMode="contain"
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
 
-    //   <TouchableOpacity
-    //     style={styles.applyBtn}
-    //     onPress={() => Linking.openURL(url)}
-    //   >
-    //     <Text style={styles.applyBtnText}>Apply for job</Text>
-    //   </TouchableOpacity>
-    // </View>
+      <TouchableOpacity
+        style={styles.applyBtn}
+        onPress={() => Linking.openURL(url)}
+      >
+        <Text style={styles.applyBtnText}>Apply for job</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
